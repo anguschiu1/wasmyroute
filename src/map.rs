717202409305bf1bf337373d08fd68e9b3de2dc9
Angus Chiu <anguschiu1@users.noreply.github.com>
2024-05-19 22:23:28 +0100

    bug fixed, gpx route printed out on map

--- src/map.rs
+++ src/map.rs
@@ -1,21 +1,10 @@
 use leaflet::{LatLng, LayerGroup, Map, MapOptions, Polyline, PolylineOptions, TileLayer};
 use log::info;
 use rand::prelude::*;
-use web_sys::{js_sys::Array, wasm_bindgen::JsValue};
+use web_sys::js_sys::Array;
 
 use crate::{Coord, Model};
 
-/// Initializes the map model with a random location and default settings.
-///
-/// This function creates a new map instance with default options, sets a random
-/// starting location if geolocation is not available, adds a tile layer and a polyline
-/// to the map, and returns a `Model` instance containing the map and its settings.
-///
-/// # Returns
-/// A `Model` instance containing:
-/// - An `Option<Map>` holding the initialized map.
-/// - The zoom level set to 5.
-/// - The randomly generated starting position.
 pub fn init() -> Model {
     // Set default map options.
     let options = MapOptions::default();
@@ -49,67 +38,12 @@ pub fn init() -> Model {
     // Add a tile layer and a polyline to the map.
     add_tile_layer(model.map.as_ref().unwrap());
 
-    let latlngs = Array::new();
-    latlngs.push(&LatLng::new(51.447148, -0.369531));
-    latlngs.push(&LatLng::new(51.846775, 0.026800));
-    latlngs.push(&LatLng::new(51.846775, 1.0));
-
-    info!(
-        "drawing line between these three points: {:?}",
-        latlngs.get(latlngs.length() - 1)
-    );
-    let options = PolylineOptions::default();
-    if let Some(position_lg) = &model.position_lg {
-        position_lg.add_layer(&Polyline::new_with_options(&latlngs, &options));
-        // position_lg.clear_layers();
-    }
-
     model
 }
-
-// Draw lines on &Map
-/// Adds a tile layer to the specified map.
-///
-/// This function adds a tile layer to the given map using the OpenStreetMap tile server.
-///
-/// # Arguments
-///
-/// * `map` - A reference to the `Map` instance to which the tile layer will be added.
 fn add_tile_layer(map: &Map) {
     TileLayer::new("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").add_to(map);
 }
 
-/// Draws a polyline on the specified map between two coordinates.
-///
-/// This function creates a polyline connecting the `from` and `to` coordinates and adds it to the given map.
-///
-/// # Arguments
-///
-/// * `map` - A reference to the `Map` instance on which the polyline will be drawn.
-/// * `from` - The starting coordinate of the polyline.
-/// * `to` - The ending coordinate of the polyline.
-fn add_polyline(model: &Model, from: Coord, to: Coord) {
-    info!("add_polyline...");
-    let options = PolylineOptions::default();
-    Polyline::new_with_options(
-        &[LatLng::new(from.lat, from.lon), LatLng::new(to.lat, to.lon)]
-            .iter()
-            .map(JsValue::from)
-            .collect::<Array>(),
-        &options,
-    )
-    .add_to(model.map.as_ref().unwrap());
-}
-
-/// Pans the map to the specified position with the current zoom level.
-///
-/// This function updates the map view to center on the given position, using the zoom level
-/// stored in the model. If the map is not initialized, the function does nothing.
-///
-/// # Arguments
-///
-/// * `model` - A reference to the `Model` instance containing the map and its settings.
-/// * `position` - The `Coord` struct representing the new position to pan to.
 pub fn pan_to_position(model: &Model, position: Coord) {
     if let Some(map) = &model.map {
         let zoom = model.zoomlevel.into();
@@ -128,7 +62,7 @@ pub fn draw_gpx_route(model: &Model) {
                 for segment in track.segments.iter() {
                     let latlngs = Array::new();
                     for point in segment.points.iter() {
-                        latlngs.push(&LatLng::new(point.point().x(), point.point().y()));
+                        latlngs.push(&LatLng::new(point.point().y(), point.point().x()));
                         info!(
                             "drawing line between these two points: {:?}",
                             latlngs.get(latlngs.length() - 1)
